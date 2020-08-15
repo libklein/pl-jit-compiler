e3fc98b0b319fba2403d9971994468a19e849dc5
Patrick Klein <patrick@libklein.com>
2020-08-15 11:29:16 +0200

    Add Parameter type to symbol table. Add find method.

--- pljit/semantic_analysis/symbol_table.cpp
+++ pljit/semantic_analysis/symbol_table.cpp
@@ -2,9 +2,15 @@
 
 using namespace pljit::semantic_analysis;
 
-auto symbol_table::insert(pljit::source_management::SourceFragment decl, symbol::symbol_type type) -> symbol_handle {
+auto symbol_table::insert(pljit::source_management::SourceFragment decl, symbol::symbol_type type, std::optional<int64_t> value) -> symbol_handle {
+    assert(type != symbol::CONSTANT || value);
     auto id = symbols.size();
-    symbols.push_back({decl, type, id, type != symbol::VARIABLE});
+    symbols.push_back({decl, type, id, type != symbol::VARIABLE, value ? *value : 0});
+    switch (type) {
+        case symbol::CONSTANT: ++number_of_constants; break;
+        case symbol::PARAMETER: ++number_of_parameters; break;
+        case symbol::VARIABLE: ++number_of_variables; break;
+    }
     return id;
 }
 
@@ -14,4 +20,35 @@ symbol& symbol_table::get(symbol_table::symbol_handle handle) {
 
 const symbol& symbol_table::get(symbol_table::symbol_handle handle) const {
     return symbols[handle];
+}
+symbol_table::size_type symbol_table::size() const {
+    return symbols.size();
+}
+
+auto symbol_table::find(std::string_view name) const -> std::optional<symbol_handle> {
+    if(auto symbol_iter = std::find_if(symbols.begin(), symbols.end(), [name](const symbol& s) { return s.get_name() == name; });
+        symbol_iter != symbols.end()) {
+        return symbol_iter->id;
+    }
+    return std::nullopt;
+}
+
+auto symbol_table::get_number_of_parameters() const -> size_type {
+    return number_of_parameters;
+}
+
+auto symbol_table::get_number_of_variables() const -> size_type {
+    return number_of_variables;
+}
+
+auto symbol_table::get_number_of_constants() const -> size_type {
+    return number_of_constants;
+}
+
+int64_t symbol::get_value() const {
+    assert(type == CONSTANT);
+    return constant_value;
+}
+std::string_view symbol::get_name() const {
+    return declaration.str();
 }
--- pljit/semantic_analysis/symbol_table.hpp
+++ pljit/semantic_analysis/symbol_table.hpp
@@ -10,24 +10,47 @@ namespace pljit::semantic_analysis {
     struct symbol {
         enum symbol_type {
             CONSTANT,
+            PARAMETER,
             VARIABLE
         };
 
         source_management::source_code::SourceFragment declaration;
         symbol_type type;
-        std::size_t id;
+        std::size_t id; // TODO nessesary?
         bool initialized;
+        int64_t constant_value;
+
+        std::string_view get_name() const;
+
+        int64_t get_value() const;
     };
 
+    /**
+     * Keeps track of symbols. Ordered: First all Params, then all Variables and then all constants
+     */
     class symbol_table {
+        public:
+        using size_type = std::vector<symbol>::size_type;
+
+        private:
         std::vector<symbol> symbols;
+        size_type number_of_variables = 0;
+        size_type number_of_parameters = 0;
+        size_type number_of_constants = 0;
         //std::unordered_map<std::string_view, symbol> identifier_mapping;
 
         public:
         using symbol_handle = std::vector<symbol>::size_type;
-        symbol_handle insert(source_management::source_code::SourceFragment decl, symbol::symbol_type type);
+        symbol_handle insert(source_management::source_code::SourceFragment decl, symbol::symbol_type type, std::optional<int64_t> initial_value);
+        std::optional<symbol_handle> find(std::string_view name) const;
         symbol& get(symbol_handle handle);
         const symbol& get(symbol_handle handle) const;
+
+        size_type size() const;
+
+        size_type get_number_of_parameters() const;
+        size_type get_number_of_variables() const;
+        size_type get_number_of_constants() const;
 
         auto begin() {
             return symbols.begin();
@@ -35,6 +58,14 @@ namespace pljit::semantic_analysis {
 
         auto end() {
             return symbols.end();
+        }
+
+        auto constants_begin() const {
+            return std::next(symbols.begin(), get_number_of_parameters() + get_number_of_variables());
+        }
+
+        auto constants_end() const {
+            return std::next(symbols.begin(), get_number_of_parameters() + get_number_of_variables() + get_number_of_constants());
         }
     };
 
