db9f2e154fd1beef75ec35f00e404127a9a1a8a4
Patrick Klein <patrick@libklein.com>
2020-08-15 11:32:39 +0200

    Add NegativeConstant to parser tests. Add helper function to generate a parse tree.

--- test/parser/TestParser.cpp
+++ test/parser/TestParser.cpp
@@ -12,52 +12,67 @@ using namespace pljit::lexer;
 
 class ParserTest : public ::testing::Test {
     protected:
-    const source_code code{"PARAM width, height, depth;\n"
-                           "VAR volume, some;\n"
-                           "CONST density = 2400;\n"
-                           "BEGIN\n"
-                           "volume := width * height * depth;\n"
-                           "some := volume + width * 10 + height;\n"
-                           "RETURN\ndensity * volume\n"
-                           "END."};
-    //const source_code code{"PARAM width, height, depth;."};
+    source_code code;
+
+    std::unique_ptr<function_defition_node> parse_code(std::string_view source_code_string) {
+        code = source_code(source_code_string);
+        lexer l(code);
+        parser parser(l);
+        return parser.parse_function_definition();
+    }
 };
 
 TEST_F(ParserTest, BasicInputTest) {
-    lexer l(code);
-    parser parser(l);
-    auto parse_tree = parser.parse_function_definition();
+    auto parse_tree = parse_code("PARAM width, height, depth;\n"
+                                 "VAR volume, some;\n"
+                                 "CONST density = 2400;\n"
+                                 "BEGIN\n"
+                                 "volume := width * height * depth;\n"
+                                 "some := volume + width * 10 + height;\n"
+                                 "RETURN\ndensity * volume\n"
+                                 "END.");
     ASSERT_TRUE(parse_tree);
 }
 
+TEST_F(ParserTest, NegativeConst) {
+    auto parse_tree = parse_code("CONST density = -2400;\n"
+                                 "BEGIN\n"
+                                 "RETURN density\n"
+                                 "END.");
+    ASSERT_FALSE(parse_tree);
+}
+
+TEST_F(ParserTest, SemicolonAfterReturn) {
+    auto parse_tree = parse_code("BEGIN\n"
+                                 "RETURN 10;\n"
+                                 "END.");
+    ASSERT_FALSE(parse_tree);
+}
+
 TEST_F(ParserTest, InputAfterProgramEnd) {
-    source_code code("PARAM width, height, depth;.PARAM width;.");
-    lexer l(code);
-    parser parser(l);
-    auto parse_tree = parser.parse_function_definition();
+    auto parse_tree = parse_code("PARAM width, height, depth;.PARAM width;.");
     ASSERT_FALSE(parse_tree);
 }
 
 TEST_F(ParserTest, MissingListItem) {
-    source_code code("PARAM width, , depth;.");
-    lexer l(code);
-    parser parser(l);
-    auto parse_tree = parser.parse_function_definition();
+    auto parse_tree = parse_code("PARAM width, , depth;.");
     ASSERT_FALSE(parse_tree);
 }
 
 TEST_F(ParserTest, InvalidTokenStream) {
-    source_code code("PARAM wid!th, height, depth;.");
-    lexer l(code);
-    parser parser(l);
-    auto parse_tree = parser.parse_function_definition();
+    auto parse_tree = parse_code("PARAM wid!th, height, depth;.");
     ASSERT_FALSE(parse_tree);
 }
 
 TEST_F(ParserTest, TestDotVisitor) {
-    lexer l(code);
-    parser parser(l);
-    auto parse_tree = parser.parse_function_definition();
+    auto parse_tree = parse_code("PARAM width, height, depth;\n"
+                                 "VAR volume, some;\n"
+                                 "CONST density = 2400;\n"
+                                 "BEGIN\n"
+                                 "volume := width * height * depth;\n"
+                                 "some := volume + width * 10 + height;\n"
+                                 "RETURN\ndensity * volume\n"
+                                 "END.");
     ASSERT_TRUE(parse_tree);
     dot_print_visitor visitor;
     parse_tree->accept(visitor);
