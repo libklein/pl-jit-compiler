115530ee3a1ec91bd98cb4b172c0dbc1126ad9e5
Patrick Klein <patrick@libklein.com>
2020-08-16 20:37:42 +0200

    Fix ExecutionContext.cpp formatting.

--- pljit/execution/ExecutionContext.cpp
+++ pljit/execution/ExecutionContext.cpp
@@ -1,18 +1,34 @@
 #include "ExecutionContext.hpp"
 
-int64_t pljit::execution::ExecutionContext::get_value(unsigned int variable_id) const {
+namespace pljit::execution {
+int64_t ExecutionContext::get_value(unsigned int variable_id) const {
     return symbols[variable_id];
 }
 
-void pljit::execution::ExecutionContext::set_value(unsigned int variable_id, int64_t value) {
+void ExecutionContext::set_value(unsigned int variable_id, int64_t value) {
     symbols[variable_id] = value;
 }
-void pljit::execution::ExecutionContext::set_result(std::optional<int64_t> res) {
+
+void ExecutionContext::set_result(std::optional<int64_t> res) {
     result = res;
 }
-std::optional<int64_t> pljit::execution::ExecutionContext::get_result() const {
+
+std::optional<int64_t> ExecutionContext::get_result() const {
     return result;
 }
-pljit::execution::ExecutionContext::operator bool() const {
+
+ExecutionContext::operator bool() const {
     return result.has_value();
 }
+
+ExecutionContext::ExecutionContext(const semantic_analysis::symbol_table& symbolTable, const std::vector<int64_t>& parameters) {
+    symbols.resize(symbolTable.size());
+    std::copy(parameters.begin(), parameters.end(), symbols.begin());
+
+    for (auto next_constant = symbolTable.constants_begin(); next_constant != symbolTable.constants_end(); ++next_constant) {
+        assert(next_constant->initialized);
+        symbols[next_constant->id] = next_constant->get_value();
+    }
+}
+
+} // namespace pljit::execution
--- pljit/execution/ExecutionContext.hpp
+++ pljit/execution/ExecutionContext.hpp
@@ -2,14 +2,10 @@
 #ifndef PLJIT_EXECUTIONCONTEXT_HPP
 #define PLJIT_EXECUTIONCONTEXT_HPP
 
+#include "pljit/semantic_analysis/symbol_table.hpp"
 #include <cstdint>
 #include <optional>
 #include <vector>
-#include "pljit/semantic_analysis/symbol_table.hpp"
-
-namespace pljit::semantic_analysis {
-    class symbol_table;
-} // namespace pljit::semantic_analysis
 
 namespace pljit::execution {
 
@@ -29,24 +25,14 @@ class ExecutionContext {
         (symbols.push_back(param), ...);
         symbols.resize(symbolTable.size());
 
-        for(auto next_constant = symbolTable.constants_begin(); next_constant != symbolTable.constants_end(); ++next_constant) {
+        for (auto next_constant = symbolTable.constants_begin(); next_constant != symbolTable.constants_end(); ++next_constant) {
             assert(next_constant->initialized);
             symbols[next_constant->id] = next_constant->get_value();
         }
     }
 
-    explicit ExecutionContext(const pljit::semantic_analysis::symbol_table& symbolTable, const std::vector<int64_t>& parameters) {
-        symbols.resize(symbolTable.size());
-        std::copy(parameters.begin(), parameters.end(), symbols.begin());
-
-        for(auto next_constant = symbolTable.constants_begin(); next_constant != symbolTable.constants_end(); ++next_constant) {
-            assert(next_constant->initialized);
-            symbols[next_constant->id] = next_constant->get_value();
-        }
-    }
-
+    explicit ExecutionContext(const pljit::semantic_analysis::symbol_table& symbolTable, const std::vector<int64_t>& parameters);
     explicit operator bool() const;
-
     void set_value(unsigned variable_id, int64_t value);
     int64_t get_value(unsigned variable_id) const;
     std::optional<int64_t> get_result() const;
